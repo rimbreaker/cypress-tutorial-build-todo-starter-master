e67cc68de06c3701e7a4081bfe30571f0bd1b871
Janek <janekkacperczyk@gmail.com>
2021-01-25 17:30:57 +0100

    first commit

---
+++ src/components/TodoApp.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
+import Footer from './Footer'
+import {saveTodo,loadTodos,destroyTodo,updateTodo} from '../lib/service'
+import {filterTodos} from '../lib/utils'
+
+
+export default class TodoApp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentTodo:'',
+      todos: []
+    }
+    this.handleNewTodoChange = this.handleNewTodoChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleDelete=this.handleDelete.bind(this)
+    this.handleToggle=this.handleToggle.bind(this)
+  }
+
+componentDidMount(){
+  loadTodos()
+    .then(({data})=>this.setState({todos:data}))
+    .catch(()=>this.setState({error:true}))
+}
+
+handleDelete(id){
+  destroyTodo(id)
+  .then(()=>this.setState({
+    todos:this.state.todos.filter(t=>t.id!==id)
+  }))
+}
+
+handleNewTodoChange(e){
+  this.setState({currentTodo:e.target.value})
+}
+
+handleSubmit(e){
+  e.preventDefault()
+  const newTodo = {name:this.state.currentTodo,isComplete:false}
+  saveTodo(newTodo)
+  .then(({data})=>this.setState({
+    todos:this.state.todos.concat(data),
+    currentTodo:''
+  }))
+  .catch(()=>this.setState({error:true}))
+}
+
+handleToggle(id){
+  const targetTodo = this.state.todos.find(t=>t.id===id)
+  const updatedTodo = {
+    ...targetTodo,
+    isComplete: !targetTodo.isComplete
+  }
+  updateTodo(updatedTodo)
+  .then(({data})=>{
+    const todos=this.state.todos.map(
+      t=>t.id===data.id ? data:t
+      )
+    this.setState({todos:todos})
+  })
+}
+
+  render () {
+    const remaining = this.state.todos.filter(t=>!t.isComplete).length
+    return (
+      <Router>
+        <div>
+          <header className="header">
+            <h1>todos</h1>
+            {this.state.error ? <span className="error">Oh no!</span>:null}
+            <TodoForm 
+            currentTodo={this.state.currentTodo}
+            handleSubmit={this.handleSubmit}
+            handleNewTodoChange={this.handleNewTodoChange}
+            />
+          </header>
+          <section className="main">
+            <Route path='/:filter?' render={({match})=>
+             <TodoList
+              todos={filterTodos(match.params.filter,this.state.todos)}
+              handleDelete={this.handleDelete} 
+              handleToggle={this.handleToggle}/>
+          }/>
+          </section>
+          <Footer remaining={remaining}/>
+        </div>
+      </Router>
+    )
+  }
+}
